bd8b6d334cfac03ddde763f8eaf0aff3d6c366b2
fl-Henry <fl.henry.jefferson@gmail.com>
2024-07-26 12:09:34 -0300

    m01l03

---
+++ m01l03/main_01.py
@@ -0,0 +1,62 @@
+"""
+comment
+"""
+import os
+import sys
+
+import requests
+
+from mymodule import my_function
+
+
+class sdf:
+    """
+
+    """
+
+    def __init__(self):
+        """
+
+        """
+
+        x = 1  # fdgshdf
+
+    def m1(self):
+        """
+
+        """
+
+        x = 1  # fdgshdf
+
+
+def fun():
+    """
+
+    :return:
+    """
+    # Default values
+    x = 1
+    y = 2
+
+    # Sum
+    z = x + y
+
+
+def fun1():
+    """
+
+    :return:
+    """
+
+
+x = 1 + 2 + 3 + 4 + 5 + 6 + 7
+
+a = 5
+print("a", a)
+b = 3
+print("b", b)
+
+result = a + b  # 8
+
+print("result", result)
+
