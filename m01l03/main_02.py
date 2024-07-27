c64976fd3b07205d442163041977c6fdc78582dd
fl-Henry <fl.henry.jefferson@gmail.com>
2024-07-27 16:38:16 -0300

    m01l03 2 group

---
+++ m01l03/main_02.py
@@ -0,0 +1,61 @@
+"""string"""
+
+a = 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10
+
+b = 5
+
+
+s1 = ("Hello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorldHello"
+      "\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorld"
+      "Hello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\nWorldHello\n"
+      "WorldHello\nWorldHello\nWorld")
+
+s1 = """Hello World\n
+Hello World
+Hello WorldHello World
+Hello World
+Hello World
+Hello World
+"""
+
+
+class A1:
+    """string"""
+
+    def __init__(self):
+        """
+
+        """
+        pass
+
+    def m1(self):
+        """
+
+        """
+        pass
+
+
+def func1(a1):
+    """
+
+    :param a1:
+    :return:
+    """
+    pass
+
+
+def func2(a1):
+    """
+
+    :param a1:
+    :return:
+    """
+
+    pass
+
+
+print(a > b)
+print((a < 0) or (a > b))
+
+if True:
+    print("True")
