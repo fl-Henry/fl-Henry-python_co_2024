5ba4e45c75e7ae5d197b376fb41c9dd76ea024d2
fl-Henry <fl.henry.jefferson@gmail.com>
2024-10-09 11:03:11 -0300

    m03 l08

---
+++ lessons/m03/m03l06/amin.py
@@ -0,0 +1,30 @@
+# from cryptography.fernet import Fernet
+#
+# # Генерация ключа
+# key = Fernet.generate_key()
+# print(key)
+#
+# cipher = Fernet(key)
+# print(cipher)
+#
+# # Шифрование данных
+# ciphertext = cipher.encrypt(b"Sensitive data")
+# print(ciphertext)
+#
+# # Расшифровка данных
+# plaintext = cipher.decrypt(ciphertext)
+# print(plaintext)
+#
+#
+#
+# # Генерация ключа
+# key = Fernet.generate_key()
+# print(key)
+#
+# cipher = Fernet(key)
+# print(cipher)
+#
+# # Расшифровка данных
+# plaintext = cipher.decrypt(ciphertext)
+# print(plaintext)
+
