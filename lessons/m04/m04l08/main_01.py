0b461de9c31238bdc8491f3f19842020d904eb68
fl-Henry <fl.henry.jefferson@gmail.com>
2024-11-08 10:23:30 -0300

    m04 l09

--- lessons/m04/m04l08/main_01.py
+++ lessons/m04/m04l08/main_01.py
@@ -1,9 +1,16 @@
 import pandas as pd
 df1 = pd.DataFrame({'A': [1, 2]}, index=['a', 'b'])
-df2 = pd.DataFrame({'B': [3, 4]}, index=['b', 'c'])
+df2 = pd.DataFrame({'A': [3, 4]}, index=['b', 'c'])
+
+print(df1)
+print()
+print(df2)
+print()
+
 
 # Выравнивание по индексу
 df1_aligned, df2_aligned = df1.align(df2, fill_value=0)
 
 print(df1_aligned)
+print()
 print(df2_aligned)
